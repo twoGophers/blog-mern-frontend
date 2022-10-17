5a84459ece621bba3bc7f1df9e101e44d9a07bb9
Vladimir <falinvlad93@rambler.ru>
2022-10-17 13:51:26 +0300

    sort post

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Grid from '@mui/material/Grid';
+import { Box, Tab, Tabs, Typography, Grid } from '@mui/material';
 
 import { Post } from '../components/Post';
 import { TagsBlock } from '../components/TagsBlock';
@@ -14,6 +12,12 @@ export const Home = () => {
   const userData = useSelector((state) => state.auth.data);
   const { posts, tags } = useSelector((state) => state.posts);
 
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const handleTabChange = (event, newTabIndex) => {
+    setTabIndex(newTabIndex);
+  };
+
   const isPostsLoading = posts.status === 'loading';
   const isTagsLoading = tags.status === 'loading';
 
@@ -22,31 +26,66 @@ export const Home = () => {
     dispatch(fetchTags());
   }, []);
 
+    
+
+    const handleTabNew = () => {
+      console.log('new');
+    };
+
+    const handleTabPopular = () => {
+      console.log('popular');
+    };
+
   return (
     <>
-      <Tabs style={{ marginBottom: 15 }} value={0} aria-label="basic tabs example">
-        <Tab label="Новые" />
-        <Tab label="Популярные" />
+      <Tabs style={{ marginBottom: 15 }} value={tabIndex} onChange={handleTabChange} aria-label="basic tabs example">
+        <Tab label="Новые" onClick={handleTabNew} />
+        <Tab label="Популярные" onClick={handleTabPopular} />
       </Tabs>
       <Grid container spacing={4}>
         <Grid xs={8} item>
-          {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) =>
-            isPostsLoading ? (
-              <Post key={index} isLoading={true} />
-            ) : (
-              <Post
-                id={obj._id}
-                title={obj.title}
-                imageUrl={obj.imageUrl ? `http://localhost:4444${obj.imageUrl}` : ''}
-                user={obj.user}
-                createdAt={obj.createdAt}
-                viewsCount={obj.viewsCount}
-                commentsCount={3}
-                tags={obj.tags}
-                isEditable={userData?._id === obj.user._id}
-              />
-            ),
-          )}
+          {tabIndex === 0 && (
+            <Box>
+              {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) =>
+                isPostsLoading ? (
+                  <Post key={index} isLoading={true} />
+                ) : (
+                  <Post
+                    id={obj._id}
+                    title={obj.title}
+                    imageUrl={obj.imageUrl ? `http://localhost:4444${obj.imageUrl}` : ''}
+                    user={obj.user}
+                    createdAt={obj.createdAt}
+                    viewsCount={obj.viewsCount}
+                    commentsCount={3}
+                    tags={obj.tags}
+                    isEditable={userData?._id === obj.user._id}
+                  />
+                ),
+              )}
+            </Box>
+          )}  
+          {tabIndex === 1 && (
+            <Box>
+              {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) =>
+                isPostsLoading ? (
+                  <Post key={index} isLoading={true} />
+                ) : (
+                  <Post
+                    id={obj._id}
+                    title={obj.title}
+                    imageUrl={obj.imageUrl ? `http://localhost:4444${obj.imageUrl}` : ''}
+                    user={obj.user}
+                    createdAt={obj.createdAt}
+                    viewsCount={obj.viewsCount}
+                    commentsCount={3}
+                    tags={obj.tags}
+                    isEditable={userData?._id === obj.user._id}
+                  />
+                ),
+              )}
+            </Box>
+          )} 
         </Grid>
         <Grid xs={4} item>
           <TagsBlock items={tags.items} isLoading={isTagsLoading} />
